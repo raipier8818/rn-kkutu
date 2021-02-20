e5ac6ff030bd928a062e8546e080a185e4ca1f8d
thak1411 <thak1411@gmail.com>
2021-02-20 22:17:50 +0900

    Update: Dictionary & Mode

--- src/content.js
+++ src/content.js
@@ -15,30 +15,78 @@ function getKeyword() {
     return document.querySelector('.jjo-display').textContent;
 }
 
-function copyLong() {
+function copyLong(reverse) {
     var key = getKeyword();
+    var key2 = getKeyword()[2];
     for (var i of rnDictLong) {
-        if (i[0] == key && window.rnSkip[i] != true) {
-            copyClipboard(i);
+        if (reverse) i = i.split('').reverse().join('');
+        if ((i[0] == key || i[0] == key2) && window.rnSkip[i] != true) {
+            window.rnSkip[i] = true;
+            if (reverse) i = i.split('').reverse().join('');
             console.log(i);
             $('[id*=UserMassage]')[0].value = i;
             $('#ChotBtn').click();
-            window.rnSkip[i] = true;
             return;
         }
     }
-    copyClipboard('');
 }
 
-function copyKill() {
-    copyLong();
+function copyKill(reverse) {
+    var key = getKeyword()[0];
+    var key2 = getKeyword()[2];
+    for (var i of rnDictKill) {
+        if (reverse) i = i.split('').reverse().join('');
+        if ((i[0] == key || i[0] == key2) && window.rnSkip[i] != true) {
+            window.rnSkip[i] = true;
+            if (reverse) i = i.split('').reverse().join('');
+            console.log(i);
+            $('[id*=UserMassage]')[0].value = i;
+            $('#ChotBtn').click();
+            return;
+        }
+    }
+    copyLong(reverse);
+}
+
+function copyEn(reverse) {
+    var key = getKeyword()[0];
+    for (var i of rnDictEn) {
+        if (reverse) i = i.split('').reverse().join('');
+        if (i[0] == key && window.rnSkip[i] != true) {
+            window.rnSkip[i] = true;
+            if (reverse) i = i.split('').reverse().join('');
+            console.log(i);
+            $('[id*=UserMassage]')[0].value = i;
+            $('#ChotBtn').click();
+            return;
+        }
+    }
+}
+
+function copykk() {
+    var key = getKeyword()[0];
+    var key2 = getKeyword()[2];
+    for (var i of rnDictKill) {
+        if ((i[0] == key || i[0] == key2) && window.rnSkip[i] != true && i.length == 3) {
+            window.rnSkip[i] = true;
+            console.log(i);
+            $('[id*=UserMassage]')[0].value = i;
+            $('#ChotBtn').click();
+            return;
+        }
+    }
 }
 
 function main() {
     initialize();
     window.onkeydown = function(e) {
-        if (e.key == "1") copyLong();
-        else if (e.key == "2") copyKill();
+        if (e.key == "1") copyLong(0);
+        else if (e.key == "2") copyKill(0);
+        else if (e.key == "3") copyLong(1);
+        else if (e.key == "4") copyKill(1);
+        else if (e.key == "5") copyEn(0);
+        else if (e.key == "6") copyEn(1);
+        else if (e.key == "7") copykk();
         else if (e.key == "0") initialize();
     }
 }
